fc60e77ae416e620649ccffaccfc4e3bc8b274b2
Alex0vSky <52796897+Alex0vSky@users.noreply.github.com>
2025-03-09 18:59:25 +0300

    reduce verbosity
    s

--- src/net/tx/DataExchanger.cpp
+++ src/net/tx/DataExchanger.cpp
@@ -4,13 +4,13 @@ namespace net::tx {
 // TODO(alex): detect disconnection in fail writeCommand_
 AwaitableBool DataExchanger::sendAndWaitResponse_(Commander::Command command, Buffer *answer) const {
 	Commander::answerSize_t answerSize = 0;
-	printf( "[DataExchanger::sendAndWaitResponse_] command: %d\n", command ); //
+	//printf( "[DataExchanger::sendAndWaitResponse_] command: %d\n", command ); //
 	if ( !co_await Commander::writeCommand_( Updater::getSocketClient( ), command, &answerSize ) ) {
 		printf( "[DataExchanger::sendAndWaitResponse_] fail writeCommand_\n" );
 		co_return false;
 	}
 	if ( 0 == answerSize ) {
-		printf( "[DataExchanger::sendAndWaitResponse_] empty\n" );
+		//printf( "[DataExchanger::sendAndWaitResponse_] empty\n" );
 		co_return false;
 	}
 	if ( !co_await async_read_( Updater::getSocketClient( ), answer, answerSize ) ) {
@@ -26,14 +26,14 @@ AwaitableVoid DataExchanger::serverIteration_() const {
 		Commander::Command remoteCommand;
 		if ( !co_await Commander::readCommand_( Updater::getSocketServer( ), &remoteCommand ) )
 			printf( "[DataExchanger::serverIteration_] fatal error, network 1" );
-		printf( "[DataExchanger::serverIteration_] remoteCommand: %d\n", remoteCommand ); //
+		//printf( "[DataExchanger::serverIteration_] remoteCommand: %d\n", remoteCommand ); //
 		const auto availableCommand = m_responces.find( remoteCommand );
 		if ( availableCommand == m_responces.end( ) ) 
 			printf( "[DataExchanger::serverIteration_] fatal error, fail m_responces.find\n" ); 
 		const Buffer data = availableCommand ->second( );
 		if ( !co_await Commander::writeData_( Updater::getSocketServer( ), data ) )
 			printf( "[DataExchanger::serverIteration_] fatal error, network 2" );
-		printf( "[DataExchanger::serverIteration_] sent: %d\n", data.size( ) ); //
+		if ( data.size( ) ) printf( "[DataExchanger::serverIteration_] sent: %d\n", data.size( ) ); //
 	}
 	co_return;
 }
