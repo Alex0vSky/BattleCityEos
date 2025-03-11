492b0c6a7f8b3e3ddb9b3db923bcaf129debcca2
Alex0vSky <52796897+Alex0vSky@users.noreply.github.com>
2025-03-11 03:04:34 +0300

    put things in order

--- src/net/cista/typeHash.h
+++ src/net/cista/typeHash.h
@@ -1,20 +1,23 @@
 #pragma once // Copyright 2025 Alex0vSky (https://github.com/Alex0vSky)
 #include "net/net.h"
 
-using hash_t = cista::hash_t;
+namespace detail {
+	using Hash = cista::hash_t;
+	using MapHash = std::map< Hash, unsigned >;
+}
 
-hash_t type_hash(net::NetPlayer const& el, hash_t h, std::map< hash_t, unsigned >& done) noexcept {
+detail::Hash type_hash(net::NetPlayer const& el, detail::Hash h, detail::MapHash &done) noexcept {
 	return cista::hash_combine( h, cista::hash( "net::NetPlayer" ) );
 }
 
-hash_t type_hash(net::NetGame::Level const& el, hash_t h, std::map< hash_t, unsigned >& done) noexcept {
+detail::Hash type_hash(net::NetGame::Level const& el, detail::Hash h, detail::MapHash &done) noexcept {
 	return cista::hash_combine( h, cista::hash( "net::NetGame::Level" ) );
 }
 
-hash_t type_hash(net::NetGame::element_t const& el, hash_t h, std::map< hash_t, unsigned >& done) noexcept {
+detail::Hash type_hash(net::NetGame::element_t const& el, detail::Hash h, detail::MapHash &done) noexcept {
 	return cista::hash_combine( h, cista::hash( "net::NetGame::element_t" ) );
 }
 
-hash_t type_hash(SpriteDataWrapper const& el, hash_t h, std::map< hash_t, unsigned >& done) noexcept {
+detail::Hash type_hash(SpriteDataWrapper const& el, detail::Hash h, detail::MapHash &done) noexcept {
 	return cista::hash_combine( h, cista::hash("SpriteDataWrapper") );
 }
